e59177ecf6822bd8547790f9cc6b89046da11573
Thomas <thknaus@htwg-konstanz.de>
2016-07-15 22:53:45 +0200

    Worked on WindowsForms

--- WalterApp/WalterApp/MainPanel.cs
+++ WalterApp/WalterApp/MainPanel.cs
@@ -16,5 +16,10 @@ namespace WalterApp
         {
             InitializeComponent();
         }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
     }
 }
--- WalterApp/WalterApp/Program.cs
+++ WalterApp/WalterApp/Program.cs
@@ -17,6 +17,7 @@ namespace WalterApp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainPanel());
+            
         }
     }
 }
