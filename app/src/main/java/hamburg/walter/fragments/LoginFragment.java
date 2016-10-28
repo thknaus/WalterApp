672d1b849aed82d888019cf6cb0c0e7be4fc4f45
thknaus <thknaus@htwg-konstanz.de>
2016-10-28 16:28:34 +0200

    User login logic.

---
+++ app/src/main/java/hamburg/walter/fragments/LoginFragment.java
@@ -0,0 +1,17 @@
+package hamburg.walter.fragments;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import hamburg.walter.R;
+
+public class LoginFragment extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_joingame);
+    }
+}
