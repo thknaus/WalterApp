d2fb8848f6beca62eb6041d97134ffc6b5e55aea
Thomas <thknaus@htwg-konstanz.de>
2016-10-30 18:26:50 +0100

    Created LoginFragment.

--- app/src/main/java/hamburg/walter/fragments/LoginFragment.java
+++ app/src/main/java/hamburg/walter/fragments/LoginFragment.java
@@ -1,17 +1,193 @@
 package hamburg.walter.fragments;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import hamburg.walter.R;
+import hamburg.walter.sync.ServerRequest;
 
 public class LoginFragment extends AppCompatActivity {
 
+    EditText email,password,res_email,code,newpass;
+    Button login,cont,cont_code,cancel,cancel1,register,forpass;
+    String emailtxt,passwordtxt,email_res_txt,code_txt,npass_txt;
+    List<NameValuePair> params;
+    SharedPreferences pref;
+    Dialog reset;
+    ServerRequest sr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_joingame);
+        setContentView(R.layout.fragment_login);
+        sr = new ServerRequest();
+
+        email = (EditText)findViewById(R.id.loginemail);
+        password = (EditText)findViewById(R.id.loginpw);
+        login = (Button)findViewById(R.id.button_login);
+        register = (Button)findViewById(R.id.button_register);
+        forpass = (Button)findViewById(R.id.button_forgotpw);
+
+        pref = getSharedPreferences("AppPref", MODE_PRIVATE);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent regactivity = new Intent(LoginFragment.this,RegisterFragment.class);
+                startActivity(regactivity);
+                finish();
+            }
+        });
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                emailtxt = email.getText().toString();
+                passwordtxt = password.getText().toString();
+                params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("email", emailtxt));
+                params.add(new BasicNameValuePair("password", passwordtxt));
+                ServerRequest sr = new ServerRequest();
+                JSONObject json = sr.getJSON("http://10.0.2.2:8080/login",params);
+                if(json != null){
+                    try{
+                        String jsonstr = json.getString("response");
+                        if(json.getBoolean("res")){
+                            String token = json.getString("token");
+                            String grav = json.getString("grav");
+                            SharedPreferences.Editor edit = pref.edit();
+                            //Storing Data using SharedPreferences
+                            edit.putString("token", token);
+                            edit.putString("grav", grav);
+                            edit.commit();
+                            Intent mainmenufragment = new Intent(LoginFragment.this,MainMenuFragment.class);
+
+                            startActivity(mainmenufragment);
+                            finish();
+                        }
+
+                        Toast.makeText(getApplication(),jsonstr,Toast.LENGTH_LONG).show();
+
+                    }catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        forpass.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                reset = new Dialog(LoginFragment.this);
+                reset.setTitle("Reset Password");
+                reset.setContentView(R.layout.fragment_resetpw);
+                cont = (Button)reset.findViewById(R.id.button_resetpw_continue);
+                cancel = (Button)reset.findViewById(R.id.button_resetpw_cancel);
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        reset.dismiss();
+                    }
+                });
+                res_email = (EditText)reset.findViewById(R.id.resetpw_email);
+
+                cont.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        email_res_txt = res_email.getText().toString();
+
+                        params = new ArrayList<NameValuePair>();
+                        params.add(new BasicNameValuePair("email", email_res_txt));
+
+                        //  JSONObject json = sr.getJSON("http://192.168.56.1:8080/api/resetpass", params);
+                        JSONObject json = sr.getJSON("http://10.0.2.2:8080/api/resetpass", params);
+
+                        if (json != null) {
+                            try {
+                                String jsonstr = json.getString("response");
+                                if(json.getBoolean("res")){
+                                    Log.e("JSON", jsonstr);
+                                    Toast.makeText(getApplication(), jsonstr, Toast.LENGTH_LONG).show();
+                                    reset.setContentView(R.layout.fragment_resetpw_code);
+                                    cont_code = (Button)reset.findViewById(R.id.button_resetpw_change);
+                                    code = (EditText)reset.findViewById(R.id.resetpw_code);
+                                    newpass = (EditText)reset.findViewById(R.id.new_pw);
+                                    cancel1 = (Button)reset.findViewById(R.id.button_resetpw_cancelcode);
+                                    cancel1.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            reset.dismiss();
+                                        }
+                                    });
+                                    cont_code.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            code_txt = code.getText().toString();
+                                            npass_txt = newpass.getText().toString();
+                                            Log.e("Code",code_txt);
+                                            Log.e("New pass",npass_txt);
+                                            params = new ArrayList<NameValuePair>();
+                                            params.add(new BasicNameValuePair("email", email_res_txt));
+                                            params.add(new BasicNameValuePair("code", code_txt));
+                                            params.add(new BasicNameValuePair("newpass", npass_txt));
+
+                                            JSONObject json = sr.getJSON("http://10.0.2.2:8080/api/resetpass/chg", params);
+                                            //   JSONObject json = sr.getJSON("http://192.168.56.1:8080/api/resetpass/chg", params);
+
+                                            if (json != null) {
+                                                try {
+
+                                                    String jsonstr = json.getString("response");
+                                                    if(json.getBoolean("res")){
+                                                        reset.dismiss();
+                                                        Toast.makeText(getApplication(),jsonstr,Toast.LENGTH_LONG).show();
+
+                                                    }else{
+                                                        Toast.makeText(getApplication(),jsonstr,Toast.LENGTH_LONG).show();
+
+                                                    }
+                                                } catch (JSONException e) {
+                                                    e.printStackTrace();
+                                                }
+                                            }
+
+                                        }
+                                    });
+                                }else{
+
+                                    Toast.makeText(getApplication(),jsonstr,Toast.LENGTH_LONG).show();
+
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                    }
+                });
+
+
+                reset.show();
+            }
+        });
     }
 }
